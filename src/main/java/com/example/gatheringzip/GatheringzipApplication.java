af73c9be39463d43271516b3c86c5705caf4b010
owo1015 <wjbin1015@gmail.com>
2023-11-15 17:02:29 +0900

    11/14 환경 세팅

---
+++ src/main/java/com/example/gatheringzip/GatheringzipApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatheringzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatheringzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatheringzipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gatheringzip/GatheringzipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatheringzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatheringzipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
